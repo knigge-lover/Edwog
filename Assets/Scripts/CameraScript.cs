30c9bee48fe67673ea5c92c5ec2caa365dbe6a5f
knigge-lover <bb3210x@icloud.com>
2025-11-23 21:05:56 +0100

    Some fixes and camera up-and-down option

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -10,14 +10,35 @@ public class CameraScript : MonoBehaviour
     [SerializeField] private InputActionReference mousePosInputAction;
     [SerializeField] private Vector3 lookAtOffset;
     private Vector3 currentLookAtOffset;
-    private float rotRetSpeed = 0.1f;
+    private float camVerticalSensitivity = 0.5f;
+    private float camVerticalBorder = 3f;
+    private float camVerticalReturnSpeed = 0.1f;
 
-    private void Update()
+    private void LateUpdate()
     {
+        float y = mousePosInputAction.action.ReadValue<Vector2>().y;
+
+        if (currentLookAtOffset.y > camVerticalBorder)
+        {
+            currentLookAtOffset.y = camVerticalBorder;
+            Debug.Log("setting down");
+        }
+        else if (currentLookAtOffset.y < -camVerticalBorder)
+        {
+            currentLookAtOffset.y = -camVerticalBorder;
+            Debug.Log("setting down");
+        }
+        else
+        {
+            currentLookAtOffset += lookAtOffset + new Vector3(0, (y * camVerticalSensitivity * Time.deltaTime) , 0);
+            currentLookAtOffset = Vector3.Lerp(currentLookAtOffset, lookAtOffset, camVerticalReturnSpeed * Time.deltaTime);
+            Debug.Log("letting");
+        }
+
     }
 
     void FixedUpdate()
     {
-        transform.LookAt(player.position - lookAtOffset);
+        transform.LookAt(player.position - currentLookAtOffset);
     }
 }
