e45216c9cc10c72afd2042530f0ed5a1d1077e4c
knigge-lover <bb3210x@icloud.com>
2026-02-07 19:48:56 +0100

    cleaned up comments in the CameraScript.cs

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -13,28 +13,6 @@ public class CameraScript : MonoBehaviour
     private float camVerticalSensitivity = .03f;
     private float camVerticalBorder = 10f;
     private float camVerticalReturnSpeed = 1f;
-    
-    /*
-    private void LateUpdate()
-    {
-        
-        float y = mousePosInputAction.action.ReadValue<Vector2>().y;
-
-        if (currentLookAtOffset.y > camVerticalBorder)
-        {
-            currentLookAtOffset.y = camVerticalBorder;
-        }
-        else if (currentLookAtOffset.y < -camVerticalBorder)
-        {
-            currentLookAtOffset.y = -camVerticalBorder;
-        }
-        else
-        {
-            currentLookAtOffset += lookAtOffset + new Vector3(0, (y * camVerticalSensitivity) , 0);
-            currentLookAtOffset = Vector3.Lerp(currentLookAtOffset, lookAtOffset, camVerticalReturnSpeed * Time.deltaTime);
-        }
-    }
-    */
 
     void LateUpdate()
     {
